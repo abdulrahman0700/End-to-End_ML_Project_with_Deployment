1e3cd6db1647823b69153216bb1b962b9d2cdb35
abdulRahman <abdulrahman0980@yahoo.com>
2024-09-15 02:32:57 +0300

    Modified_Files

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,request,jsonify,url_for,render_template,app
 import numpy as np
 import pandas as pd
 import json
+
 app = Flask(__name__)
 
 model = joblib.load('player_rating_model.pkl')
@@ -12,7 +13,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
-def predict():
+def predicts():
     data = request.json['data']
     print(data)
     Pandas_data = pd.DataFrame(data,index=[0])
@@ -22,5 +23,16 @@ def predict():
     print(pred)
     return pred
 
-if __name__ == '__main__' :
+@app.route("/predict",methods=["POST"])
+def predication():
+    data = [d for d in request.form.values()]
+    print(data)
+    Pandas_data = pd.DataFrame({"Teams":data[0],"Seasons":data[1],"Players":data[2],"Matches":int(data[3]),"Goals":int(data[4]),"Assists":int(data[5])},index=[0])
+    print(Pandas_data)
+    predicts = model.predict(Pandas_data)
+    print(predicts)
+    return render_template("home.html",prediction_text="The Season Rate Prediction is {}".format(predicts[0]))
+    
+
+if __name__ == '__main__':
     app.run(debug=True)
