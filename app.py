682ea02aca6b68206260befd9bec6eb97714e969
abdulrahman0700 <135911160+abdulrahman0700@users.noreply.github.com>
2026-08-23 15:07:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,24 +25,24 @@ This Function is for testing API on postman
 def predicts():
     data = request.json['data']
     listed_data = [values_data for values_data in data.values()]
-    print(data)
-    print(listed_data)
+    # print(data)
+    # print(listed_data)
     Pandas_data = pd.DataFrame(data,index=[0])
     db.insert_data(0,listed_data[0],listed_data[1],listed_data[2],listed_data[3],listed_data[4],listed_data[5])
     prediction = model.predict(Pandas_data)
-    print(prediction)
+    # print(prediction)
     pred = json.dumps({'prediction': prediction.tolist()})
-    print(pred)
+    # print(pred)
     return pred
 
 @app.route("/predict",methods=["POST"])
 def predication():
     data = [d for d in request.form.values()]
-    print(data)
+    # print(data)
     Pandas_data = pd.DataFrame({"Teams":data[0],"Seasons":data[1],"Players":data[2],"Matches":int(data[3]),"Goals":int(data[4]),"Assists":int(data[5])},index=[0])
-    print(Pandas_data)
+    # print(Pandas_data)
     predicts = model.predict(Pandas_data)
-    print(predicts)
+    # print(predicts)
     return render_template("home.html",prediction_text="The Season Rate Prediction is {}".format(predicts[0]))
     
 
